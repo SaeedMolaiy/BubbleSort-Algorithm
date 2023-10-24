dadadea54dd1a9040195c5d6ccc205a9b9ec3a8d
Saeed Molaee <saeedmolaiy.dev@gmail.com>
2023-10-24 08:53:06 +0330

    BubbleSort Algorithm

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,40 @@
+﻿namespace BubbleSort;
+
+internal class BubbleSort
+{
+    public static void BubbleSortArray(int[] arr)
+    {
+        var n = arr.Length;
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            var swapped = false;
+
+            for (var j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] <= arr[j + 1])
+                {
+                    continue;
+                }
+
+                (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                swapped = true;
+            }
+
+            if (!swapped)
+                break;
+
+            for (var k = n - i - 1; k > 0; k--)
+            {
+                if (arr[k] < arr[k - 1])
+                {
+                    (arr[k], arr[k - 1]) = (arr[k - 1], arr[k]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+Console.WriteLine("Original Array:");
+PrintArray(arr);
+
+BubbleSort.BubbleSort.BubbleSortArray(arr);
+
+Console.WriteLine("\nSorted Array:");
+PrintArray(arr);
+
+static void PrintArray(IEnumerable<int> arr)
+{
+    foreach (var num in arr)
+    {
+        Console.Write(num + " ");
+    }
+
+    Console.WriteLine();
+}
